d13343ad49e640af087f5a86ea8b79544852fb03
Mauricio Robayo <rfmajo@gmail.com>
2019-10-24 17:50:26 -0500

    Add books logic to display and remove

--- js/index.js
+++ js/index.js
@@ -1,35 +1,72 @@
 const BOOKS_URL = 'https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json';
 const form = document.querySelector('#library-form');
+const newBookBtn = document.querySelector('#new-book');
 const library = [];
 
 class Book {
-  constructor({ author, title, pages }) {
+  constructor({
+    author,
+    title,
+    pages,
+    imageLink,
+    read = false,
+  }) {
+    this.readSatusBtn = null;
     this.author = author;
     this.title = title;
-    this.pages = pages;
+    this.pages = `${pages}`;
+    this.imageLink = imageLink;
+    this.read = !!read;
   }
 
-  render() {
+  toggleBookStatus() {
+    this.read = !this.read;
+    this.readSatusBtn.classList.toggle('active');
+  }
+
+  render(index) {
     const bookContainer = document.createElement('div');
+    bookContainer.id = index;
     Object.keys(this).forEach((key) => {
-      const data = document.createElement('div');
-      data.innerText = this[key];
-      bookContainer.appendChild(data);
+      if (typeof this[key] === 'string') {
+        const data = document.createElement('div');
+        data.innerText = this[key];
+        bookContainer.appendChild(data);
+      }
     });
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.class = 'delete-book';
+    const readBtn = document.createElement('button');
+    this.readSatusBtn = readBtn;
+    readBtn.addEventListener('click', this.toggleBookStatus);
+    readBtn.innerText = 'Read';
+    bookContainer.append(readBtn, deleteBtn);
     return bookContainer;
   }
 }
+
+let deleteBook;
 
 function render() {
   const libraryContainer = document.querySelector('#library');
   const libContainer = document.createElement('div');
   libContainer.id = 'library';
-  library.forEach((book) => {
-    const bookElement = book.render();
+  library.forEach((book, index) => {
+    const bookElement = book.render(index);
     libContainer.appendChild(bookElement);
   });
   document.body.replaceChild(libContainer, libraryContainer);
+  libContainer.addEventListener('click', deleteBook);
 }
+
+deleteBook = (event) => {
+  if (event.target.class === 'delete-book') {
+    const index = event.target.parentElement.id;
+    library.splice(index, 1);
+    render();
+  }
+};
 
 function addBookToLibrary(book) {
   library.push(new Book(book));
@@ -42,6 +79,11 @@ function cleanFields() {
   });
 }
 
+function bringUpForm() {
+  cleanFields();
+  form.classList.add('active');
+}
+
 function processBook(event) {
   event.preventDefault();
   const book = {};
@@ -50,15 +92,17 @@ function processBook(event) {
     book[key] = value;
   });
   addBookToLibrary(book);
-  cleanFields();
+  form.classList.remove('active');
 }
 
 form.addEventListener('submit', processBook);
+newBookBtn.addEventListener('click', bringUpForm);
 
 fetch(BOOKS_URL)
   .then((response) => response.json())
   .then((data) => {
-    data.slice(0, 10).forEach((book) => {
+    data.slice(0, 4).forEach((book) => {
+      book.imageLink = `https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/${book.imageLink}`;
       addBookToLibrary(book);
     });
   });
