ac7e66304f9b0cac26b33572a02aa3878a4bb1f0
Mauricio Robayo <rfmajo@gmail.com>
2019-10-24 14:50:27 -0500

    Add initial config files

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  rules: {
+    'no-shadow': 0,
+    'no-param-reassign': 0,
+    'eol-last': 0,
+  },
+}
