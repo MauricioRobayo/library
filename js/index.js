41546cb5eeee43807543f7c5e6493feff99df921
oscarnava <oscarnava@gmail.com>
2019-10-25 12:15:52 -0500

    Add local storage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
 const BOOKS_URL = 'https://raw.githubusercontent.com/benoitvallon/100-best-books/master/';
+const STORAGE_PREFIX = 'MauOsc';
 const form = document.querySelector('#library-form');
 const newBookBtn = document.querySelector('#new-book');
 const library = [];
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22
+      // Firefox
+      || e.code === 1014
+      // test name field too, because code might not be present
+      // everything except Firefox
+      || e.name === 'QuotaExceededError'
+      // Firefox
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && (storage && storage.length !== 0);
+  }
+}
 
 class Book {
   constructor({
@@ -43,9 +68,30 @@ class Book {
     bookContainer.append(readBtn, deleteBtn);
     return bookContainer;
   }
+
+  get json() {
+    return JSON.stringify(this);
+  }
+
+  static dbBookKey(idx) {
+    return `${STORAGE_PREFIX}.book.${idx}`;
+  }
 }
 
-let deleteBook;
+function storeLibrary() {
+  library.forEach((book, idx) => {
+    localStorage.setItem(Book.dbBookKey(idx), book.json);
+  });
+}
+
+function loadLibrary(addBookFn) {
+  let idx = 0;
+  let key = Book.dbBookKey(idx);
+  while (localStorage.getItem(key)) {
+    addBookFn(JSON.parse(localStorage.getItem(key)));
+    key = Book.dbBookKey(idx += 1);
+  }
+}
 
 function render() {
   const libraryContainer = document.querySelector('#library');
@@ -56,21 +102,21 @@ function render() {
     libContainer.appendChild(bookElement);
   });
   document.body.replaceChild(libContainer, libraryContainer);
-  libContainer.addEventListener('click', deleteBook);
+  libContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete-book')) {
+      const index = event.target.parentElement.id;
+      library.splice(index, 1);
+      render();
+      storeLibrary();
+    }
+    if (event.target.classList.contains('read-book')) {
+      const index = event.target.parentElement.id;
+      library[index].readStatus = !library[index].readStatus;
+      render();
+      storeLibrary();
+    }
+  });
 }
-
-deleteBook = (event) => {
-  if (event.target.classList.contains('delete-book')) {
-    const index = event.target.parentElement.id;
-    library.splice(index, 1);
-    render();
-  }
-  if (event.target.classList.contains('read-book')) {
-    const index = event.target.parentElement.id;
-    library[index].readStatus = !library[index].readStatus;
-    render();
-  }
-};
 
 function addBookToLibrary(book) {
   library.push(new Book(book));
@@ -96,18 +142,25 @@ function processBook(event) {
   });
   addBookToLibrary(book);
   render();
+  storeLibrary();
   form.classList.remove('active');
 }
 
 form.addEventListener('submit', processBook);
 newBookBtn.addEventListener('click', bringUpForm);
 
-fetch(`${BOOKS_URL}/books.json`)
-  .then(response => response.json())
-  .then((data) => {
-    data.slice(0, 4).forEach((book) => {
-      book.imageLink = `${BOOKS_URL}/static/${book.imageLink}`;
-      addBookToLibrary(book);
+if (storageAvailable('localStorage') && localStorage.getItem(Book.dbBookKey(0))) {
+  loadLibrary(addBookToLibrary);
+  render();
+} else {
+  fetch(`${BOOKS_URL}/books.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.slice(0, 4).forEach((book) => {
+        book.imageLink = `${BOOKS_URL}/static/${book.imageLink}`;
+        addBookToLibrary(book);
+      });
+      render();
+      storeLibrary();
     });
-    render();
-  });
+}
